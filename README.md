f3ee629616934ed48558e5597874599693e5361b
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-02-09 23:58:48 -0600

    created server.js dependencies, express app/middleware setup, routes and listener

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 
 
 NPM Packages Used:
-Body-Parser: https://www.npmjs.com/package/body-parser
-Express: https://www.npmjs.com/package/express
-Path: https://www.npmjs.com/package/path
+- Body-Parser: https://www.npmjs.com/package/body-parser
+- Express: https://www.npmjs.com/package/express
+- Path: https://www.npmjs.com/package/path
 
 Author:
 Arnold R Zimmerman Jr
--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 var bodyParser = require('body-parser');
 var express = require('express');
 var path = require('path');
+
+// Sets up the Express App
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+
+// Router
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Listener
+app.listen(PORT, function() {
+  console.log('App listening on PORT: ' + PORT);
+});
