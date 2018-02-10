9af78f1ce6febc4cf1671abe82157867c8b9e89b
Arnold R Zimmerman Jr <arnoldrz2@gmail.com>
2018-02-10 00:40:41 -0600

    added in last names for characters from 'The Office'

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 // Friends Data Storage Area
 var friendsArray = [
         {
-          "name":"Michael",
+          "name":"Michael Scott",
           "photo":"http://www.top5bin.com/wp-content/uploads/2013/04/Michael-in-Fun-Run-michael-scott-1534318-1212-682.jpg",
           "scores":[
               5,
@@ -17,7 +17,7 @@ var friendsArray = [
             ]
         },
         {
-          "name":"Pam",
+          "name":"Pam Beesly",
           "photo":"https://www.gannett-cdn.com/-mm-/27f6664fb8ef48299f06ee96e2d6771613497b2a/c=170-0-2830-2000&r=x404&c=534x401/local/-/media/2017/02/08/USATODAY/USATODAY/636221410316183057-A01-v2-REAR17.jpg",
           "scores":[
               3,
@@ -33,7 +33,7 @@ var friendsArray = [
             ]
         },
         {
-          "name":"Jim",
+          "name":"Jim Halpert",
           "photo":"https://pbs.twimg.com/media/C_2DE1rV0AA7mOA.jpg",
           "scores":[
               5,
@@ -49,7 +49,7 @@ var friendsArray = [
             ]
         },
         {
-        "name":"Dwight",
+        "name":"Dwight Schrute",
         "photo":"https://az616578.vo.msecnd.net/files/2016/06/24/636023262738743231122108782_Dwight-Schrute-1.jpg",
         "scores":[
             1,
@@ -65,7 +65,7 @@ var friendsArray = [
           ]
       },
       {
-      "name":"Kelly",
+      "name":"Kelly Kapoor",
       "photo":"https://vignette.wikia.nocookie.net/degrassi/images/4/4d/Kelly_kapoor.jpg/revision/latest?cb=20130630062809",
       "scores":[
           5,
